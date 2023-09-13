46ced8c2474cc1d967dfb17baf48980a17823ca6
“fuscarebaixad0” <leticia.gb@aluno.ifsc.edu.br>
2023-09-13 17:06:54 -0300

    MainVeiculu, Veiculo

--- lista04/src/ifsc/MainVeiculo.java
+++ lista04/src/ifsc/MainVeiculo.java
@@ -1,5 +1,33 @@
 package ifsc;
 
+import java.util.ArrayList;
+
 public class MainVeiculo {
 
+	public static void main(String[] args) {
+
+		ArrayList<Veiculo> veiculos = new ArrayList<>();
+
+		Veiculo veiculo1 = new Veiculo();
+		veiculo1.setNumeroRodas(4);
+		veiculo1.setFabricante("Fiat");
+		veiculo1.setCor("Preto");
+		veiculos.add(veiculo1);
+		Veiculo veiculo2 = new Veiculo();
+		veiculo2.setNumeroRodas(2);
+		veiculo2.setFabricante("McLaren");
+		veiculo2.setCor("Branco");
+		veiculos.add(veiculo2);
+		Veiculo veiculo3 = new Veiculo();
+		veiculo3.setNumeroRodas(4);
+		veiculo3.setFabricante("Ford");
+		veiculo3.setCor("vermelho");
+		veiculos.add(veiculo3);
+		for (Veiculo veiculo : veiculos) {
+			System.out.println("Número de Rodas: " + veiculo.getNumeroRodas());
+			System.out.println("Fabricante: " + veiculo.getFabricante());
+			System.out.println("Cor: " + veiculo.getCor());
+			System.out.println();
+		}
+	}
 }
--- lista04/src/ifsc/Veiculo.java
+++ lista04/src/ifsc/Veiculo.java
@@ -2,4 +2,49 @@ package ifsc;
 
 public class Veiculo {
 
+	private String nome;
+	private Integer ano;
+	private Integer numeroRodas;
+	private String fabricante;
+	private String cor;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+
+	public Integer getNumeroRodas() {
+		return numeroRodas;
+	}
+
+	public void setNumeroRodas(Integer numeroRodas) {
+		this.numeroRodas = numeroRodas;
+	}
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
 }
